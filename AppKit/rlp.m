99fd31fa2f199f73ee7a7fa9fca1e3f27bc2e361
Marco Rolappe <m_rolappe@gmx.net>
2022-03-28 18:49:41 +0200

    WIP misc AppKit constants

---
+++ AppKit/rlp.m
@@ -0,0 +1,18 @@
+#import <AppKit/AppKitExport.h>
+#import <Foundation/Foundation.h>
+
+APPKIT_EXPORT NSString *const NSApplicationLaunchUserNotificationKey = @"NSApplicationLaunchUserNotificationKey";
+
+typedef NSString *NSAttributedStringKey;
+
+APPKIT_EXPORT NSAttributedStringKey NSGlyphInfoAttributeName = @"NSGlyphInfo";
+
+
+typedef NSString *NSAccessibilityNotificationUserInfoKey;
+
+const NSAccessibilityNotificationUserInfoKey NSAccessibilityAnnouncementKey = @"NSAccessibilityAnnouncementKey";
+
+
+typedef NSString *NSBindingOption;
+
+NSBindingOption NSConditionallySetsHiddenBindingOption = @"NSConditionallySetsHiddenBindingOption";
