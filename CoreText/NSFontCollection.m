5e329ec7a10c5f21ac83a272c826640bd617f13c
Marco Rolappe <m_rolappe@gmx.net>
2022-03-28 18:37:45 +0200

    WIP NSFontCollection

---
+++ CoreText/NSFontCollection.m
@@ -0,0 +1,14 @@
+#import <Foundation/Foundation.h>
+
+@interface NSFontCollection : NSObject
+@end
+
+@implementation NSFontCollection
+- (NSMethodSignature *)methodSignatureForSelector:(SEL)aSelector {
+    return [NSMethodSignature signatureWithObjCTypes: "v@:"];
+}
+
+- (void)forwardInvocation:(NSInvocation *)anInvocation {
+    NSLog(@"Stub called: %@ in %@", NSStringFromSelector([anInvocation selector]), [self class]);
+}
+@end
