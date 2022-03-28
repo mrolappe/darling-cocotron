5ea6aa5404e4f7898b096e7d764704cd0f229d55
Marco Rolappe <m_rolappe@gmx.net>
2022-03-28 18:36:25 +0200

    WIP CoreData stuff

---
+++ CoreData/NSPersistentContainer.m
@@ -0,0 +1,14 @@
+#import <Foundation/NSObject.h>
+
+@interface NSPersistentContainer : NSObject
+@end
+
+@implementation NSPersistentContainer
+- (NSMethodSignature *)methodSignatureForSelector:(SEL)aSelector {
+    return [NSMethodSignature signatureWithObjCTypes: "v@:"];
+}
+
+- (void)forwardInvocation:(NSInvocation *)anInvocation {
+    NSLog(@"Stub called: %@ in %@", NSStringFromSelector([anInvocation selector]), [self class]);
+}
+@end
