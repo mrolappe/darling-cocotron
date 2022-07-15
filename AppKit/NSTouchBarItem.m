4447efdc5d1fa02c85bdb88d9d6d6e7ad2e6188a
Marco Rolappe <215132+mrolappe@users.noreply.github.com>
2022-07-15 22:36:41 +0200

    AppKit stub; NSCandidateListTouchBarItem

--- AppKit/NSTouchBarItem.m
+++ AppKit/NSTouchBarItem.m
@@ -43,3 +43,19 @@ NSTouchBarItemIdentifier const NSTouchBarItemIdentifierCharacterPicker =
 }
 
 @end
+
+
+@interface NSCandidateListTouchBarItem<__covariant CandidateType> : NSTouchBarItem
+@end
+
+@implementation NSCandidateListTouchBarItem
+- (NSMethodSignature *)methodSignatureForSelector:(SEL)aSelector
+{
+    return [NSMethodSignature signatureWithObjCTypes: "v@:"];
+}
+
+- (void)forwardInvocation:(NSInvocation *)anInvocation
+{
+    NSLog(@"Stub called: %@ in %@", NSStringFromSelector([anInvocation selector]), [self class]);
+}
+@end
